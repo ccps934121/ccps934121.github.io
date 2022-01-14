52d4fa8caa3bd0c2bd8d9291e8540dfe2ef87bd8
emma <emma@hcc.gitlab.local>
2022-01-14 11:43:40 +0800

    add dea

---
+++ pwaForDea/precache-manifest.1ae4c4d9e3bb2c803fd446bbf67dc9bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbe1dac457c0f7c34dd",
+    "url": "css/app.a5a1d849.css"
+  },
+  {
+    "revision": "74e03f16975099bd4d8cd87afc2cf39c",
+    "url": "index.html"
+  },
+  {
+    "revision": "3bbe1dac457c0f7c34dd",
+    "url": "js/app.bc8a87ff.js"
+  },
+  {
+    "revision": "26f5238f832a1e812c5a",
+    "url": "js/chunk-vendors.954f23cf.js"
+  },
+  {
+    "revision": "3868a87186592ee3caa21fc69400dc7f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
