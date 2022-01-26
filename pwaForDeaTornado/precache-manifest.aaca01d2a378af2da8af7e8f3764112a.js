f474bb5c4b674e20bf998ee1ccce68019de76c1c
emma <emma@hcc.gitlab.local>
2022-01-26 10:35:39 +0800

    add dea and tornado

---
+++ pwaForDeaTornado/precache-manifest.aaca01d2a378af2da8af7e8f3764112a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2367b770e295da225389",
+    "url": "css/app.de7acb3c.css"
+  },
+  {
+    "revision": "55f77abe773b1c893b3e",
+    "url": "css/chunk-vendors.9ed213e8.css"
+  },
+  {
+    "revision": "a407005166855f6a62e123ea00b82c63",
+    "url": "index.html"
+  },
+  {
+    "revision": "2367b770e295da225389",
+    "url": "js/app.0b627c09.js"
+  },
+  {
+    "revision": "55f77abe773b1c893b3e",
+    "url": "js/chunk-vendors.adf78771.js"
+  },
+  {
+    "revision": "3868a87186592ee3caa21fc69400dc7f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
