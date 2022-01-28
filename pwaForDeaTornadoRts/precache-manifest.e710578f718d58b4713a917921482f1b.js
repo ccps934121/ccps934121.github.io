235a2ae9f63f64a1057d5cfb8733c8411c70ea32
emma <emma@hcc.gitlab.local>
2022-01-29 01:02:36 +0800

    add rts

---
+++ pwaForDeaTornadoRts/precache-manifest.e710578f718d58b4713a917921482f1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192bb9b4459636836f54",
+    "url": "css/app.d82d9b88.css"
+  },
+  {
+    "revision": "d0e68ad44a7b23db7055",
+    "url": "css/chunk-vendors.9ed213e8.css"
+  },
+  {
+    "revision": "5926bd6b8fbcb468708724b5f33433a0",
+    "url": "index.html"
+  },
+  {
+    "revision": "192bb9b4459636836f54",
+    "url": "js/app.b46eb671.js"
+  },
+  {
+    "revision": "d0e68ad44a7b23db7055",
+    "url": "js/chunk-vendors.239efe7b.js"
+  },
+  {
+    "revision": "3868a87186592ee3caa21fc69400dc7f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
